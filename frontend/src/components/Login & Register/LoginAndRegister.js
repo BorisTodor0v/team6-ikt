8b9350065222978ffff5704837ee72bd54e27875
BorisTodor0v <b.todorov52@gmail.com>
2024-04-24 17:27:55 +0200

    Refactor React project Add Header and Login/Register Page

---
+++ frontend/src/components/Login & Register/LoginAndRegister.js
@@ -0,0 +1,58 @@
+import React from "react";
+import * as Components from './Components';
+
+const LoginAndRegister = (props) => {
+    const [signIn, toggle] = React.useState(true);
+    return(
+        <Components.Container>
+            <Components.SignUpContainer signinIn={signIn}>
+                <Components.Form>
+                    <Components.Title>Креирај профил</Components.Title>
+                    <Components.Input type='text' placeholder='Име и презиме' />
+                    <Components.Input type='email' placeholder='E-пошта' />
+                    <Components.Input type='password' placeholder='Лозинка' />
+                    <Components.Button>РЕГИСТРИРАЈ СЕ</Components.Button>
+                </Components.Form>
+            </Components.SignUpContainer>
+
+            <Components.SignInContainer signinIn={signIn}>
+                <Components.Form>
+                    <Components.Title>Најава</Components.Title>
+                    <Components.Input type='email' placeholder='E-пошта' />
+                    <Components.Input type='password' placeholder='Лозинка' />
+                    <Components.Anchor href='#'>Заборавена лозинка?</Components.Anchor>
+                    <Components.Button>НАЈАВИ СЕ</Components.Button>
+                </Components.Form>
+            </Components.SignInContainer>
+
+            <Components.OverlayContainer signinIn={signIn}>
+                <Components.Overlay signinIn={signIn}>
+
+                    <Components.LeftOverlayPanel signinIn={signIn}>
+                        <Components.Title>ДОБРЕДОЈДЕ НАЗАД!</Components.Title>
+                        <Components.Paragraph>
+                            Веќе си корисник? Најави се
+                        </Components.Paragraph>
+                        <Components.GhostButton onClick={() => toggle(true)}>
+                            НАЈАВИ СЕ
+                        </Components.GhostButton>
+                    </Components.LeftOverlayPanel>
+
+                    <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>Потребен ти е негувател?</Components.Title>
+                        <Components.Paragraph>
+                            Доколку се уште немаш креирано профил на Kindly, придружи ни се!
+                        </Components.Paragraph>
+                        <Components.GhostButton onClick={() => toggle(false)}>
+                            РЕГИСТРИРАЈ СЕ
+                        </Components.GhostButton>
+                    </Components.RightOverlayPanel>
+
+                </Components.Overlay>
+            </Components.OverlayContainer>
+
+        </Components.Container>
+    )
+}
+
+export default LoginAndRegister;
